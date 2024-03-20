558ab97b170fe5dd2d55044c73437f85774101d7
Pimplay-Aakash <pimplayaakash@gmail.com>
2024-03-20 12:18:38 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Pressable } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Pressable>
+        <Text>Hello</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default App
